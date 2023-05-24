f6eb1618a3961ceb5a9cbddf791001ef7029de0c
ajb1102 <abiss29@gmail.com>
2023-05-23 22:03:20 -0400

    Fade

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const images = document.querySelectorAll("#slideshow-container img");
 let currentImageIndex = 0;
 
 function showNextImage() {
-    images[currentImageIndex].style.display = "none";
+    const currentImage = images[currentImageIndex];
+    currentImage.style.opacity = 0; // Fade out the current image
+
     currentImageIndex = (currentImageIndex + 1) % images.length;
-    images[currentImageIndex].style.display = "block";
+    const nextImage = images[currentImageIndex];
+    nextImage.style.opacity = 1; // Fade in the next image
+
+    setTimeout(() => {
+        currentImage.style.display = "none"; // Hide the current image
+        nextImage.style.display = "block"; // Show the next image
+    }, 1000); // Transition duration (in milliseconds)
 }
 
+images[currentImageIndex].style.display = "block"; // Show the first image
 setInterval(showNextImage, 3000); // Change image every 3 seconds
